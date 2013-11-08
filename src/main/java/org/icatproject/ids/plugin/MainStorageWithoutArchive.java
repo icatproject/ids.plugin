f6d5353c2fc6fe00a031d23cb3542eb4a70ca2e2
Steve Fisher <dr.s.m.fisher@gmail.com>
2013-11-08 14:08:44 +0000

    Added cache cleanup

--- src/main/java/org/icatproject/ids/plugin/MainStorageWithoutArchive.java
+++ src/main/java/org/icatproject/ids/plugin/MainStorageWithoutArchive.java
@@ -1,5 +1,6 @@
 package org.icatproject.ids.plugin;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
@@ -11,32 +12,42 @@ public abstract class MainStorageWithoutArchive implements MainStorageInterface 
 
 	/**
 	 * This is a dummy implementation
+	 * 
+	 * @throws IOException
 	 */
 	@Override
-	public void delete(DsInfo dsInfo) {
+	public void delete(DsInfo dsInfo) throws IOException {
+		throw new IOException("Please do not request an archive plugin in the ids.properties file");
 	}
 
 	/**
 	 * This is a dummy implementation
+	 * 
+	 * @throws IOException
 	 */
 	@Override
-	public boolean exists(DsInfo dsInfo) {
-		return false;
+	public boolean exists(DsInfo dsInfo) throws IOException {
+		throw new IOException("Please do not request an archive plugin in the ids.properties file");
 	}
 
 	/**
 	 * This is a dummy implementation
+	 * 
+	 * @throws IOException
 	 */
 	@Override
-	public List<String> getLocations(DsInfo dsInfo) {
-		return null;
+	public List<String> getLocations(DsInfo dsInfo) throws IOException {
+		throw new IOException("Please do not request an archive plugin in the ids.properties file");
 	}
 
 	/**
 	 * This is a dummy implementation
+	 * 
+	 * @throws IOException
 	 */
 	@Override
-	public void putUnchecked(DsInfo dsInfo, String dfName, InputStream is) {
+	public void putUnchecked(DsInfo dsInfo, String dfName, InputStream is) throws IOException {
+		throw new IOException("Please do not request an archive plugin in the ids.properties file");
 	}
 
 }
---
+++ src/main/java/org/icatproject/ids/plugin/MainStorageWithoutArchiveReadOnly.java
@@ -0,0 +1,29 @@
+package org.icatproject.ids.plugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * This is a partial implementation of a MainStorageInterface providing dummy implementations of
+ * those operations not needed when no archive storage is being used and when the readOnly flag has
+ * been set in the ids.properties file
+ */
+public abstract class MainStorageWithoutArchiveReadOnly extends MainStorageWithoutArchive {
+
+	/**
+	 * This is a dummy implementation
+	 */
+	@Override
+	public void delete(String location) throws IOException {
+		throw new IOException("Please add \"readOnly\" to the ids.properties file");
+	}
+
+	/**
+	 * This is a dummy implementation
+	 */
+	@Override
+	public DfInfo put(DsInfo dsInfo, String name, InputStream inputStream) throws IOException {
+		throw new IOException("Please add \"readOnly\" to the ids.properties file");
+	}
+
+}
