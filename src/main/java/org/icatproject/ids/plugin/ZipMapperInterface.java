61c8edcc2e8e6c3770d03a83f5ca9277d6742625
Steve Fisher <dr.s.m.fisher@gmail.com>
2014-05-16 09:50:25 +0000

    Prepare for 1.1.0

--- src/main/java/org/icatproject/ids/plugin/ZipMapperInterface.java
+++ src/main/java/org/icatproject/ids/plugin/ZipMapperInterface.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 /**
  * 
- * Handle the generation and of the full entry name for a zip file entry and derive the
- * Datafile.name from it.
+ * Handle the generation of the full entry name for a zip file entry and derive the Datafile.name
+ * from it.
  * 
  */
 public interface ZipMapperInterface {
@@ -14,11 +14,15 @@ public interface ZipMapperInterface {
 	 * Derive full entry name for the file specified by the dsInfo and dfInfo
 	 * 
 	 * @param dsInfo
+	 *            describes the dataset for which the name must be defined
 	 * 
 	 * @param dfInfo
+	 *            describes the datafile for which the name must be defined
 	 * 
 	 * @return full entry name
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
+	 *             if unable to do the job
 	 */
 	public String getFullEntryName(DsInfo dsInfo, DfInfo dfInfo) throws IOException;
 
@@ -26,9 +30,13 @@ public interface ZipMapperInterface {
 	 * Derive Datafile.name as known in ICAT for the zip file entry fullEntryName
 	 * 
 	 * @param fullEntryName
+	 *            is the full entry name in the zip file from which the information must be
+	 *            extracted
 	 * 
-	 * @return Datafile.name or null if not possible
+	 * @return Datafile.name
+	 * 
 	 * @throws IOException
+	 *             if unable to do the job
 	 */
 	public String getFileName(String fullEntryName) throws IOException;
 
